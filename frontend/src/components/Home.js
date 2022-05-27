5fa54f02a247c6b8a9d136c5b5ff87900880953b
Yoav Zinger <47221812y@gmail.com>
2022-05-27 11:09:38 +0300

    fixed barchart to work for the agreed json scheme

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { BarChart } from './charts/barchart';
 
 const Home = (props) => {
   
@@ -9,6 +10,9 @@ const Home = (props) => {
       <Typography variant="h4" component="div" gutterBottom>
         OpenShift-Sentinel - World's First SIEM For RedHat OpenShift!
       </Typography>
+
+      <BarChart title="arad vs marik" legendPosition="left" datasetLabels={['arad', 'marik']} datasetData={[[4,2,5], [3,4,7]]} labels={['morning', 'afternoon', 'night']}></BarChart>
+      
 </Box>
   );
 };

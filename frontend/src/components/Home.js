23f715d26375ef8275a5d3336674875d92d9589d
Yoav Zinger <47221812y@gmail.com>
2022-05-27 15:03:39 +0300

    fixede barchart to the agreed dataset scheme

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,14 +5,27 @@ import { BarChart } from './charts/barchart';
 
 const Home = (props) => {
   
-
+const chartData = {
+  arad: {
+    '/api/route/1/2/3':5,
+    '/api/route/1/3/5':30,
+    '/api/another/route/1/v1/hello':60,
+    '/api/api?/wow/route3/4/5': 55
+  },
+  yoav: {
+    '/api/route/1/2/3':5,
+    '/api/route/1/3/5':30,
+    '/api/api?/wow/route3/4/5': 55,
+    '/api/power/rangers/5':30
+  }
+}
   return (
   <Box sx={{ width: '100%', maxWidth: 500 }}>
       <Typography variant="h4" component="div" gutterBottom>
         OpenShift-Sentinel - World's First SIEM For RedHat OpenShift!
       </Typography>
 
-    
+    <BarChart legendPosition='top' title='api router chart' chartData={chartData} chartLables={['/api/api?/wow/route3/4/5']}></BarChart>
 </Box>
   );
 };
